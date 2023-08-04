82750949154f6a4b0ebf38eecfb0b6c48e04ed63
jrupinski <rupinskijakub@gmail.com>
2023-08-04 23:19:44 +0200

    Add basic display window

--- lib/chip-8/components/display.rb
+++ lib/chip-8/components/display.rb
@@ -1,7 +1,10 @@
-class Display
-  attr_accessor :display
+require 'gosu'
 
-  def initialize
-    @display = Array.new(64 * 32, 0)
+class Display < Gosu::Window
+  def initialize(width: 64, height: 32, resizable: true)
+    super width, height, resizable: resizable
+    self.caption = 'Chip-8 Emulator by Rupcio - test'
   end
 end
+
+Display.new.show
