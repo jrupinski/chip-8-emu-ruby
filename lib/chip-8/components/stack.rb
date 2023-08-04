b252a9dc51bde029eb95aeaf4e220922a56691f4
jrupinski <rupinskijakub@gmail.com>
2023-08-04 23:42:53 +0200

    Update stack class to properly initialize

--- lib/chip-8/components/stack.rb
+++ lib/chip-8/components/stack.rb
@@ -1,7 +1,9 @@
+# A stack for 16-bit addresses, which is used to
+# call subroutines/functions and return from them
 class Stack
   attr_accessor :stack
 
   def initialize
-
+    @stack = []
   end
 end
