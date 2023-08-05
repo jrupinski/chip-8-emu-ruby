c2333811024cbb53a04db5c86430d699e9531cc7
jrupinski <rupinskijakub@gmail.com>
2023-08-05 23:12:27 +0200

    Add sound timer implementation

--- lib/chip-8/components/sound_timer.rb
+++ lib/chip-8/components/sound_timer.rb
@@ -1,7 +1,17 @@
 class SoundTimer
-  attr_accessor :sound_timer
+  attr_reader :sound_timer
 
   def initialize
-
+    @sound_timer = 0
   end
+
+  # Makes system "beep" sound
+  def beep
+    print "\a"
+  end
+
+  def should_beep?
+    @sound_timer.positive?
+  end
+
 end
