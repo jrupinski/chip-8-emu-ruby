0e736e9546f5b4937f088fb1125150fb901d24a1
jrupinski <rupinskijakub@gmail.com>
2023-08-05 23:12:03 +0200

    Add delay timer implementation

--- lib/chip-8/components/delay_timer.rb
+++ lib/chip-8/components/delay_timer.rb
@@ -1,7 +1,11 @@
 class DelayTimer
-  attr_accessor :delay_timer
+  attr_reader :delay_timer
 
   def initialize
+    @delay_timer = 0
+  end
 
+  def decrement!
+    @delay_timer -= 1 unless @delay_timer.zero?
   end
 end
