82e01fa123b3528e45924aad98d6826365c0e8ff
jrupinski <rupinskijakub@gmail.com>
2023-08-06 18:12:20 +0200

    Add 60Hz refresh rate to display

--- lib/chip-8/components/display.rb
+++ lib/chip-8/components/display.rb
@@ -1,8 +1,10 @@
 require 'gosu'
 
+REFRESH_RATE = 1000 / 60 # 60Hz (60 times per second)
+
 class Display < Gosu::Window
-  def initialize(width: 64, height: 32, resizable: true)
-    super width, height, resizable: resizable
+  def initialize(width: 64, height: 32, resizable: true, interval: REFRESH_RATE)
+    super width, height, resizable: resizable, update_interval: interval
     self.caption = 'Chip-8 Emulator by Rupcio - test'
   end
 end
