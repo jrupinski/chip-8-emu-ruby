df0727cc2d51d38fce18044a5074dd9853488e81
jrupinski <rupinskijakub@gmail.com>
2023-08-08 22:04:54 +0200

    Add basic chip-8 main class

---
+++ lib/chip-8/chip_8.rb
@@ -0,0 +1,28 @@
+require 'components/delay_timer'
+require 'components/display'
+require 'components/font'
+require 'components/index_register'
+require 'components/keyboard'
+require 'components/memory'
+require 'components/program_counter'
+require 'components/register'
+require 'components/stack'
+require 'components/sound_timer'
+
+class Chip8
+  def initialize
+    # TODO
+  end
+
+  def fetch
+    # TODO
+  end
+
+  def decode
+    # TODO
+  end
+
+  def execute
+    # TODO
+  end
+end
